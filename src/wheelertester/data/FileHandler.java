c6057111e6dfb5de74ba8366c1487abfaace4528
gswheeler <SilentRaptor22@gmail.com>
2015-11-23 13:37:32 -0500

    Project created
    Initial project files

---
+++ src/wheelertester/data/FileHandler.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wheelertester.data;
+
+/**
+ *
+ * @author Greg
+ */
+public class FileHandler extends wheeler.generic.data.FileHandler {
+    
+}
