398eca89891280a3157ea861d2b37e8dd19da6e7
gswheeler <SilentRaptor22@gmail.com>
2015-11-30 11:03:06 -0500

    AddFunc: GetTestFolder
    Added functions for getting folderpaths in the Wheeler Tester program
    folder that are unique to the test calling the function (allows a unique
    environment to be set up for each test).

--- src/wheelertester/data/FileHandler.java
+++ src/wheelertester/data/FileHandler.java
@@ -5,10 +5,41 @@
  */
 package wheelertester.data;
 
+import wheeler.generic.data.LogicHandler;
+
 /**
- *
- * @author Greg
+ * Handle file operations common across project tests
  */
 public class FileHandler extends wheeler.generic.data.FileHandler {
+    
+    protected static String programFolder = "C:\\Program Files\\Wheeler\\Wheeler Tester";
+    protected static boolean programFolderTested = false;
+    
+    /**
+     * Get a folder in the Wheeler Tester folder named after the method signature
+     * @param argTag An extra tag to add if the same function tests multiple situations
+     * @param indirection Zero starting from the function that creates the folder
+     * @return The filepath of a folder in Wheeler Tester named after the calling function
+     * @throws Exception If there's an issue creating the Wheeler Tester folder or getting the method signature
+     */
+    protected static String getTestFolder(String argTag, int indirection) throws Exception{
+        return getTestFolder(indirection + 1) + "-" + argTag;
+    }
+    /**
+     * Get a folder in the Wheeler Tester folder named after the method signature
+     * @param indirection Zero starting from the function that creates the folder
+     * @return The filepath of a folder in Wheeler Tester named after the calling function
+     * @throws Exception If there's an issue creating the Wheeler Tester folder or getting the method signature
+     */
+    protected static String getTestFolder(int indirection) throws Exception{
+        // Make sure we can access the Wheeler Tester folder
+        if(!programFolderTested){
+            testProgramFolder(programFolder, null);
+            programFolderTested = true;
+        }
+        
+        // Compose and return the filepath
+        return composeFilepath(programFolder, LogicHandler.getCallingMethod(indirection + 1));
+    }
     
 }
