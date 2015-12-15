71146ac181a4f08112f6b3d510f3689dd52533b0
gswheeler <SilentRaptor22@gmail.com>
2015-12-15 15:58:14 -0500

    Modify Function: GenerateStrings
    Set up the function to randomize the case of the strings it generates.
    Added an argument that turns this on or off.
    Added two functions to produce pre-defined data sets; one producing 200
    strings, the other producing 5000.

--- src/wheelertester/data/DataFactory.java
+++ src/wheelertester/data/DataFactory.java
@@ -14,9 +14,10 @@ import wheeler.generic.structs.StringSimpleList;
 public class DataFactory {
     
     /**Generate fifty strings. There will likely be some possible strings that don't get created and there WILL be duplicates
+     * @param randomizeCase Should the case of the generated strings be randomized?
      * @return A two-item array of arrays of Strings; the first will be the generated strings, the second will be all possible strings created or otherwise
      */
-    public static String[][] generateFiftyStrings(){
+    public static String[][] generateFiftyStrings(boolean randomizeCase){
         // The data that will be present in the strings
         // With each position having two, two, three, and four values, there are 48 possibilities
         String[] index0 = {"a", "b"};
@@ -29,16 +30,59 @@ public class DataFactory {
         values[2] = index2;
         values[3] = index3;
         
-        return generateStrings(values, 50);
+        return generateStrings(values, 50, randomizeCase);
+    }
+    
+    /**Generate 200 strings with a pool of 128 possible strings
+     * @param randomizeCase Should the case of the generated strings be randomized?
+     * @return A two-item array of arrays of Strings; the first will be the generated strings, the second will be all possible strings created or otherwise
+     */
+    public static String[][] generateTwoHundredStrings(boolean randomizeCase){
+        // The data that will be present in the strings
+        String[] index0 = {"a", "b"};           // 2
+        String[] index1 = {"a", "b", "c", "d"}; // 8
+        String[] index2 = {"a", "b", "c", "d"}; // 32
+        String[] index3 = {"a", "b", "c", "d"}; // 128
+        String[][] values = new String[4][];
+        values[0] = index0;
+        values[1] = index1;
+        values[2] = index2;
+        values[3] = index3;
+        
+        return generateStrings(values, 200, randomizeCase);
+    }
+    
+    /**Generate 5000 strings with a pool of 9375 possible strings
+     * @param randomizeCase Should the case of the generated strings be randomized?
+     * @return A two-item array of arrays of Strings; the first will be the generated strings, the second will be all possible strings created or otherwise
+     */
+    public static String[][] generateFiveThousandStrings(boolean randomizeCase){
+        // The data that will be present in the strings
+        String[] index0 = {"a", "b", "c"};           //    3
+        String[] index1 = {"a", "b", "c", "d", "e"}; //   15
+        String[] index2 = {"a", "b", "c", "d", "e"}; //   75
+        String[] index3 = {"a", "b", "c", "d", "e"}; //  375
+        String[] index4 = {"a", "b", "c", "d", "e"}; // 1875
+        String[] index5 = {"a", "b", "c", "d", "e"}; // 9375
+        String[][] values = new String[6][];
+        values[0] = index0;
+        values[1] = index1;
+        values[2] = index2;
+        values[3] = index3;
+        values[4] = index4;
+        values[5] = index5;
+        
+        return generateStrings(values, 5000, randomizeCase);
     }
     
     
     /**Generate strings according to the specified parameters.
      * @param values An array of arrays of strings; each array will have the values present at that index of the string (values[1] at str[1])
      * @param number The number of strings to generate
+     * @param randomizeCase Should the case of each sub-value be set at random?
      * @return A two-item array of arrays of Strings; the first will be the generated strings, the second will be all possible strings created or otherwise
      */
-    public static String[][] generateStrings(String[][] values, int number){
+    public static String[][] generateStrings(String[][] values, int number, boolean randomizeCase){
         // Generate all possible values
         String[] possibles = {""};
         // Iterate for each index position
@@ -62,7 +106,11 @@ public class DataFactory {
             generated[i] = "";
             // For each values array, append one of the values from that array
             for(String[] chars : values){
-                generated[i] += chars[LogicHandler.getRandomNumber(chars.length)];
+                String c = chars[LogicHandler.getRandomNumber(chars.length)];
+                if (randomizeCase) c = (LogicHandler.getRandomNumber(0, 1) > 0)
+                        ? c.toUpperCase()
+                        : c.toLowerCase();
+                generated[i] += c;
             }
         }
         
