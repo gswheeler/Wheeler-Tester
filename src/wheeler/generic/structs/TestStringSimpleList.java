06271f418ba4d18cc3a1a4c8be8c4b3fd4ed2412
gswheeler <SilentRaptor22@gmail.com>
2015-12-15 11:09:20 -0500

    Add Class: TestStringSimpleList
    Added a class for providing test methods for StringSimpleList.

---
+++ src/wheeler/generic/structs/TestStringSimpleList.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wheeler.generic.structs;
+
+/**
+ *
+ * @author Greg
+ */
+public class TestStringSimpleList extends wheeler.generic.structs.StringSimpleList {
+    
+}
