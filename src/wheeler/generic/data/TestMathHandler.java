95b902f3630f814f74ee2b0d3310ae5604bd59c5
gswheeler <SilentRaptor22@gmail.com>
2016-01-14 14:33:25 -0500

    Added a test class for MathHandler
    Added a class that allows tests to call the protected primeNumbersCache
    function

---
+++ src/wheeler/generic/data/TestMathHandler.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wheeler.generic.data;
+
+/**
+ * A class with special functions for setting protected fields according to test values
+ */
+public class TestMathHandler extends wheeler.generic.data.MathHandler{
+    
+    public static String primeNumbersCache(){
+        return wheeler.generic.data.MathHandler.primeNumbersCache();
+    }
+    
+}
